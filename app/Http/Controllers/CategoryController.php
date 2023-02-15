70308f9ae85eacc01d041a8ea1517c45f65c8f6c
SAURABH <sgupta8mz@gmail.com>
2023-02-15 23:06:03 +0530

    third commit

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -30,9 +30,14 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
-        //
+        $category=new Category;
+        $category->name=$request->name;
+        $category->save();
+
+        return ["message"=>"Category Created"];
+
     }
 
     /**
